1db7800be3e345f8b3ff67edb9b564ae87406129
Akhil T <fslearn@vc.com>
2026-04-28 11:15:03 +0530

    Deployment prep: updated CORS to wildcard and set frontend API URL

--- Todo-List-backend-spring/src/main/java/com/WebProjects/Todo_List/Controller/TodoController.java
+++ Todo-List-backend-spring/src/main/java/com/WebProjects/Todo_List/Controller/TodoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/tasks")
 @AllArgsConstructor
-@CrossOrigin(origins="http://localhost:5173/")
+@CrossOrigin(origins="*")
 public class TodoController {
 
     private TodoService todoService;
